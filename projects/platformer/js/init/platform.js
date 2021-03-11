802cda3978d5cf0546c937b0d9091f34d38cdf99
JalonLollis <jalon.lollis@newharmonyhigh.org>
2021-03-11 06:38:46 +0000

    add portfolio.html file

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -43,7 +43,7 @@
         createPlatform(300, 700, 0.8);
         createPlatform(520, 535, 0.5);     
         createPlatform(800, 450, 0.7);
-        createPlatform(700, 250);
+        createPlatform(550, 250);
 
         
         
